68ef0b011cd7e19b6914759e27f95e8595775a2c
Krishnapriyaajith2002 <krishnapriyaajith002@gmail.com>
2024-11-13 16:56:10 +0530

    my second version

--- empapp/models.py
+++ empapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from rest_framework.authtoken.models import Token
-# Create your models here.
+# Create your  wonderful models here.
 #create a reciever for the signal 'post_save' for the user model
 #once its created create a token for that user
 @receiver(post_save, sender=settings.AUTH_USER_MODEL) 
